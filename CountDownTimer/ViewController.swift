f97a037447a69d1e47aec8bb95f43f2d4fca5a2f
PHAM NGOC QUYNH <phamngocquynh.mail@gmail.com>
2016-01-26 21:59:45 +0700

    Play with Text, Negative Timer
    Insert 2 function: playWithText(),  negativeTimer()

--- CountDownTimer/ViewController.swift
+++ CountDownTimer/ViewController.swift
@@ -28,16 +28,52 @@ class ViewController: UIViewController {
         _ = NSTimer.scheduledTimerWithTimeInterval(0.1, target: self, selector: "updateTimer", userInfo: nil, repeats: true)
     }
     func updateTimer(){
-        let date = fromDateToDate(datePicker.date)
-        yearMonthDayLabel.text = "\(date.year)years  \(date.month)month  \(date.day)days"
+        var date = fromDateToDate(datePicker.date)
+        let (year, month, day) = playWithText(date)
+        var str: String = ""
+        (date,str) = negativeTimer(date, str: str)
+        yearMonthDayLabel.text = "\(str) \(date.year)\(year)  \(date.month)\(month)  \(date.day)\(day)"
         timeLabel.text = "\(date.hour):\(date.minute):\(date.second)"
     }
-   
+    
+    func playWithText(date: NSDateComponents)->(String, String, String){
+        var year : String = "year"
+        var month : String = "month"
+        var day : String = "day"
+        
+        if date.year % 2 == 0 {
+            year = "years"
+        }
+        
+        if date.month % 2 == 0 {
+            month = "months"
+        }
+        
+        if date.day % 2 == 0 {
+            day = "days"
+        }
+        
+        return (year,month,day)
+    }
+    
+    func negativeTimer (component : NSDateComponents, var str : String) -> (NSDateComponents,String) {
+        if component.second < 0 {
+            component.second *= -1
+            component.minute *= -1
+            component.hour *= -1
+            component.day *= -1
+            component.month *= -1
+            component.year *= -1
+            str = "From:"
+        }
+        return (component,str)
+    }
+    
     func fromDateToDate(day : NSDate) -> NSDateComponents{
         let date = NSDate()
         let unitFlags: NSCalendarUnit = [.Second,.Minute,.Hour, .Day, .Month, .Year]
         let component = NSCalendar.currentCalendar().components(unitFlags, fromDate: date, toDate: day, options: NSCalendarOptions.WrapComponents)
-        return component
+               return component
     }
 }
 
